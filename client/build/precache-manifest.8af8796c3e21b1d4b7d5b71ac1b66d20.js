4abdb5c90ecfa2c9e1962876ce52fe26e7db684e
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-09-11 13:07:17 -0500

    deploying

---
+++ client/build/precache-manifest.8af8796c3e21b1d4b7d5b71ac1b66d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7c24da0763daef8fa0fe3ffafc5ab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a82e308efa826e54843",
+    "url": "/static/css/2.e66dd83d.chunk.css"
+  },
+  {
+    "revision": "a0047fe5de8d71cd9160",
+    "url": "/static/css/main.5fbf6d3e.chunk.css"
+  },
+  {
+    "revision": "4a82e308efa826e54843",
+    "url": "/static/js/2.95b9f1ec.chunk.js"
+  },
+  {
+    "revision": "5599e968862052a1e150ba50f6354fc6",
+    "url": "/static/js/2.95b9f1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0047fe5de8d71cd9160",
+    "url": "/static/js/main.0a2a6e3e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

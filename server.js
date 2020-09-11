de45207f5287f917aa5c5de6af47a6234833dd9b
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-09-11 13:08:09 -0500

    deploying

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
